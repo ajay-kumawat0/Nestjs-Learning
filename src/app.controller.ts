3f3312d1a5c51d0cb2e8f34a25d1f60d651fd368
Ajay Kumawat <ajay@elemensissoftech.com>
2024-07-30 15:59:45 +0530

    Token generated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth/auth.service';
 
 @Controller('app')
 export class AppController {
+  constructor(private readonly authService: AuthService) {}
+
   @Get()
   @UseGuards(AuthGuard('local'))
   getUser(@Request() req) {
-    return req.user;
+    return `Token = ${this.authService.generateToken(req.user)}`;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { PassportLocalStrategy } from './passport.local.strategy';
 import { UserModule } from 'src/users/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'NestJs-Api',
+      signOptions: {
+        expiresIn: '60s',
+      },
+    }),
+  ],
   controllers: [],
-  providers: [PassportLocalStrategy],
+  providers: [PassportLocalStrategy, AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
